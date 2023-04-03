7d7a909e97c6b04eb40eb73ed3819e1ea989a95c
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-04-02 22:25:22 -0400

    update

--- puzzle.py
+++ puzzle.py
@@ -12,7 +12,8 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Biconditional(AKnight, And(AKnight, AKnave)),
+    Biconditional(AKnave, Not(And(AKnight, AKnave)))
 )
 
 # Puzzle 1
